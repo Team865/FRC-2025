d8ed85bbf2142a957831ba44147164b2a470927f
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-01-05 14:03:14 -0500

    Add drive controls

--- src/main/java/ca/warp7/frc2025/RobotContainer.java
+++ src/main/java/ca/warp7/frc2025/RobotContainer.java
@@ -4,15 +4,63 @@
 
 package ca.warp7.frc2025;
 
+import ca.warp7.frc2025.commands.DriveCommands;
+import ca.warp7.frc2025.subsystems.drive.Drive;
+import ca.warp7.frc2025.subsystems.drive.GyroIO;
+import ca.warp7.frc2025.subsystems.drive.GyroIOPigeon2;
+import ca.warp7.frc2025.subsystems.drive.ModuleIOSim;
+import ca.warp7.frc2025.subsystems.drive.ModuleIOTalonFX;
+import ca.warp7.frc2025.subsystems.generated.TunerConstants;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
+    // Subsystems
+    private final Drive drive;
+
+    // Controller
+    private final CommandXboxController controller = new CommandXboxController(0);
+
     public RobotContainer() {
+        switch (Constants.currentMode) {
+            case REPLAY:
+            case REAL:
+                // Real robot, instantiate hardware IO implementations
+                drive = new Drive(
+                        new GyroIOPigeon2(),
+                        new ModuleIOTalonFX(TunerConstants.FrontLeft),
+                        new ModuleIOTalonFX(TunerConstants.FrontRight),
+                        new ModuleIOTalonFX(TunerConstants.BackLeft),
+                        new ModuleIOTalonFX(TunerConstants.BackRight));
+                break;
+
+            case SIM:
+                // Sim robot, instantiate physics sim IO implementations
+                drive = new Drive(
+                        new GyroIO() {},
+                        new ModuleIOSim(TunerConstants.FrontLeft),
+                        new ModuleIOSim(TunerConstants.FrontRight),
+                        new ModuleIOSim(TunerConstants.BackLeft),
+                        new ModuleIOSim(TunerConstants.BackRight));
+                break;
+            default:
+                drive = null;
+        }
+
         configureBindings();
     }
 
-    private void configureBindings() {}
+    /**
+     * Use this method to define your button->command mappings. Buttons can be created by
+     * instantiating a {@link GenericHID} or one of its subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureBindings() {
+        drive.setDefaultCommand(DriveCommands.joystickDrive(
+                drive, () -> -controller.getLeftY(), () -> -controller.getLeftX(), () -> -controller.getRightX()));
+    }
 
     public Command getAutonomousCommand() {
         return Commands.print("No autonomous command configured");
