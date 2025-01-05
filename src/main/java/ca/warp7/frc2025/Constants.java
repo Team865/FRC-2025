38369f8488bb7686a202365411f9d979da1ea98d
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-01-04 22:41:00 -0500

    Create constants file

---
+++ src/main/java/ca/warp7/frc2025/Constants.java
@@ -0,0 +1,21 @@
+package ca.warp7.frc2025;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+/**
+ * Class containing global constants for the whole robot
+ */
+public final class Constants {
+    public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : Mode.SIM;
+
+    public static enum Mode {
+        // Running on a real robot
+        REAL,
+
+        //  Running in the simulator
+        SIM,
+
+        // Replaying log file on the robot
+        REPLAY
+    }
+}
