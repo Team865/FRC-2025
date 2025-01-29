496ada1d84f09419c7272931b97161b647d963bb
Elliot <eteaisme@gmail.com>
2025-01-29 13:53:42 -0500

    Fix shlop

--- src/main/java/ca/warp7/frc2025/BuildConstants.java
+++ src/main/java/ca/warp7/frc2025/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "FRC-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 34;
-    public static final String GIT_SHA = "32924ae481ff16951bb972820bbf56dc2866ec8d";
-    public static final String GIT_DATE = "2025-01-29 12:00:46 EST";
+    public static final int GIT_REVISION = 39;
+    public static final String GIT_SHA = "4b93f18743d84a169347dcab5bfbaa4b5a460606";
+    public static final String GIT_DATE = "2025-01-29 13:50:00 EST";
     public static final String GIT_BRANCH = "Climber";
-    public static final String BUILD_DATE = "2025-01-29 12:06:18 EST";
-    public static final long BUILD_UNIX_TIME = 1738170378739L;
+    public static final String BUILD_DATE = "2025-01-29 13:52:34 EST";
+    public static final long BUILD_UNIX_TIME = 1738176754827L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
--- src/main/java/ca/warp7/frc2025/subsystems/Climber/ClimberIO.java
+++ src/main/java/ca/warp7/frc2025/subsystems/Climber/ClimberIO.java
@@ -1,22 +1,20 @@
 package ca.warp7.frc2025.subsystems.Climber;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import org.littletonrobotics.junction.AutoLog;
 
-import edu.wpi.first.math.geometry.Rotation2d;
-
 public interface ClimberIO {
-    @AutoLog 
+    @AutoLog
     static class ClimberIOInputs {
         public double velocityRadPerSec = 0.0;
         public Rotation2d position = new Rotation2d();
         public double driveAppliedVolts = 0.0;
         public double driveCurrentAmps = 0.0;
- 
     }
 
-    default void updateInputs(ClimberIOInputs inputs){}
+    default void updateInputs(ClimberIOInputs inputs) {}
 
-    default void setVoltage(double volts){}
-    default void setPosition(double position){}
+    default void setVoltage(double volts) {}
 
+    default void setPosition(double position) {}
 }
