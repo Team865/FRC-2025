d8a398e303c23a872d9e33e9e27f546737d977f0
Elliot <eteaisme@gmail.com>
2025-01-29 12:33:19 -0500

    Finished Climber Interface

--- src/main/java/ca/warp7/frc2025/subsystems/Climber/ClimberIO.java
+++ src/main/java/ca/warp7/frc2025/subsystems/Climber/ClimberIO.java
@@ -1,3 +1,22 @@
 package ca.warp7.frc2025.subsystems.Climber;
 
-public interface ClimberIO {}
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public interface ClimberIO {
+    @AutoLog 
+    static class ClimberIOInputs {
+        public double velocityRadPerSec = 0.0;
+        public Rotation2d position = new Rotation2d();
+        public double driveAppliedVolts = 0.0;
+        public double driveCurrentAmps = 0.0;
+ 
+    }
+
+    default void updateInputs(ClimberIOInputs inputs){}
+
+    default void setVoltage(double volts){}
+    default void setPosition(double position){}
+
+}
