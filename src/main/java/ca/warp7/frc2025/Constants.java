d05253c18b17ef9a0b9aff5a5ed3299682b8c9a5
Elliot <eteaisme@gmail.com>
2025-01-29 13:07:46 -0500

    Added Empty Climber Constants

--- src/main/java/ca/warp7/frc2025/Constants.java
+++ src/main/java/ca/warp7/frc2025/Constants.java
@@ -1,5 +1,6 @@
 package ca.warp7.frc2025;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.RobotBase;
 
 /**
@@ -23,5 +24,11 @@ public final class Constants {
 
     public static final class Drivetrain {
         public static final double PERIOD = 0.02;
+    }
+
+    public static final class Climber {
+        public static final double CLIMBER_ARM_LENGTH_METERS = Units.inchesToMeters(0.0);
+        public static final double CLIMBER_DRUM_RADIUS_METERS = Units.inchesToMeters(0.0);
+        public static final double CLIMBER_GEAR_RATIO = 0.0;
     }
 }
