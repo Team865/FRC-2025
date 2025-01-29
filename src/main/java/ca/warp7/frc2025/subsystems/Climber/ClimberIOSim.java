ddb844ffe991d4155b4484382d00e82630c3c276
Elliot <eteaisme@gmail.com>
2025-01-29 14:44:18 -0500

    Renamed ClimberIOSim

--- src/main/java/ca/warp7/frc2025/subsystems/Climber/ClimberIOSim.java
+++ src/main/java/ca/warp7/frc2025/subsystems/Climber/ClimberIOSim.java
@@ -12,32 +12,29 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 
-public class CLimberIOSim implements ClimberIO {
+public class ClimberIOSim implements ClimberIO {
     private final SingleJointedArmSim armSim = new SingleJointedArmSim(
-        DCMotor.getKrakenX60(2), 
-        Climber.CLIMBER_GEAR_RATIO,
-        0.0,
-        Climber.CLIMBER_ARM_LENGTH_METERS,
-        Units.degreesToRadians(0.0),
-        Units.degreesToRadians(360.0),
-        true,
-        Units.degreesToRadians(0.0)
-    );
-    
+            DCMotor.getKrakenX60(2),
+            Climber.CLIMBER_GEAR_RATIO,
+            0.0,
+            Climber.CLIMBER_ARM_LENGTH_METERS,
+            Units.degreesToRadians(0.0),
+            Units.degreesToRadians(360.0),
+            true,
+            Units.degreesToRadians(0.0));
+
     private double climberAppliedVolts;
     private final ArmFeedforward feedforward = new ArmFeedforward(0, 0, 0);
     private final ProfiledPIDController pid =
-      new ProfiledPIDController(5.0, 0.0, 0.0, new TrapezoidProfile.Constraints(10.0, 10.0));
-
+            new ProfiledPIDController(5.0, 0.0, 0.0, new TrapezoidProfile.Constraints(10.0, 10.0));
 
     @Override
     public void updateInputs(ClimberIOInputsAutoLogged inputs) {
         armSim.update(0.02);
         inputs.velocityRadPerSec = RadiansPerSecond.of(armSim.getAngleRads()).in(RadiansPerSecond);
         inputs.position = Rotation2d.fromRadians(armSim.getAngleRads());
-        inputs.climberCurrentAmps = armSim.getCurrentDrawAmps(); 
-        inputs.climberAppliedVolts = climberAppliedVolts; 
-
+        inputs.climberCurrentAmps = armSim.getCurrentDrawAmps();
+        inputs.climberAppliedVolts = climberAppliedVolts;
     }
 
     @Override
@@ -45,15 +42,13 @@ public class CLimberIOSim implements ClimberIO {
         double climberAppliedVolts = MathUtil.clamp(volts, -12, -12);
         armSim.setInputVoltage(volts);
     }
+
     @Override
     public void setPosition(final double position) {
-    setVoltage(
-        pid.calculate(
-                armSim.getAngleRads(),
-                Math.asin(
-                    (Units.rotationsToRadians(position)
-                            * Climber.CLIMBER_DRUM_RADIUS_METERS)
-                        / Climber.CLIMBER_ARM_LENGTH_METERS))
-            + feedforward.calculate(pid.getSetpoint().position, pid.getSetpoint().velocity));
-  }
+        setVoltage(pid.calculate(
+                        armSim.getAngleRads(),
+                        Math.asin((Units.rotationsToRadians(position) * Climber.CLIMBER_DRUM_RADIUS_METERS)
+                                / Climber.CLIMBER_ARM_LENGTH_METERS))
+                + feedforward.calculate(pid.getSetpoint().position, pid.getSetpoint().velocity));
+    }
 }
