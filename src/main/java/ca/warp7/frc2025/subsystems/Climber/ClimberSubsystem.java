a2ec867c8a09f351979401fcfe5707faf1b3e959
Elliot <eteaisme@gmail.com>
2025-01-29 14:41:53 -0500

    Added ClimberSubsystem

--- src/main/java/ca/warp7/frc2025/subsystems/Climber/ClimberSubsystem.java
+++ src/main/java/ca/warp7/frc2025/subsystems/Climber/ClimberSubsystem.java
@@ -1,3 +1,20 @@
 package ca.warp7.frc2025.subsystems.Climber;
 
-public class ClimberSubsystem {}
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClimberSubsystem extends SubsystemBase{
+    private final ClimberIO io; 
+    private final ClimberIOInputsAutoLogged inputs = new ClimberIOInputsAutoLogged();
+    
+    public ClimberSubsystem(ClimberIO io) {
+        this.io = io; 
+    }
+
+   @Override
+   public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Climber", inputs);
+   } 
+}
