9fa0b64774e25c9b1ee028bd9b334cc85493a1f5
Elliot <eteaisme@gmail.com>
2025-01-31 12:47:18 -0500

    January 26th Spare Changes
    Finsihed fixing up the climber sim.'

--- src/main/java/ca/warp7/frc2025/commands/ClimberCommands.java
+++ src/main/java/ca/warp7/frc2025/commands/ClimberCommands.java
@@ -1,7 +1,3 @@
 package ca.warp7.frc2025.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
-
-public class ClimberCommands {
-    
-}
+public class ClimberCommands {}
--- src/main/java/ca/warp7/frc2025/subsystems/Climber/ClimberIO.java
+++ src/main/java/ca/warp7/frc2025/subsystems/Climber/ClimberIO.java
@@ -6,15 +6,11 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ClimberIO {
     @AutoLog
     static class ClimberIOInputs {
-        public Rotation2d position = new Rotation2d();
-        
-        public double pivotLeftVelocityRadPerSec = 0.0;
-        public double pivotLeftVolts = 0.0;
-        public double pivotLeftCurrentAmps = 0.0;
+        public Rotation2d pivotRotation = new Rotation2d();
+        public double pivotVelocityRadPerSec = 0.0;
 
-        public double pivotRightVelocityRadPerSec = 0.0;
-        public double pivotRightVolts = 0.0;
-        public double pivotRightCurrentAmps = 0.0;
+        public double[] pivotAppliedVolts = new double[] {};
+        public double[] pivotCurrentAmps = new double[] {};
 
         public double climbIntakeVelocityRadPerSec = 0.0;
         public double climbIntakeVolts = 0.0;
@@ -23,14 +19,11 @@ public interface ClimberIO {
 
     default void updateInputs(ClimberIOInputsAutoLogged inputs) {}
 
-    default void setPivotVoltage(double left, double right) {}
+    default void setPivotVoltage(double volts) {}
 
-    default void setPivotSetpoint(Rotation2d rotation) {}
-
-    default void setPivotPosition(Rotation2d roatation) {}
+    default void setPivotPosition(Rotation2d rotation) {}
 
     default void resetPivotPosition(Rotation2d rotation) {}
-    
-    default void setClimbIntakeVoltage(double volts) {}
 
+    default void setIntakeVoltage(double volts) {}
 }
