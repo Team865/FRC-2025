0abf4c7d5bb47dfebc1b9fdf9ac5f3d8dba61585
Elliot <eteaisme@gmail.com>
2025-02-06 15:39:10 -0500

    Added Climber Real implementation

--- src/main/java/ca/warp7/frc2025/subsystems/Climber/ClimberIO.java
+++ src/main/java/ca/warp7/frc2025/subsystems/Climber/ClimberIO.java
@@ -1,18 +1,21 @@
 package ca.warp7.frc2025.subsystems.Climber;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.geometry.Rotation2d;
 
 public interface ClimberIO {
     @AutoLog
     static class ClimberIOInputs {
-        public Rotation2d pivotRotation = new Rotation2d();
-        public double pivotVelocityRadPerSec = 0.0;
+        public boolean motorConnected = false;
+        public boolean followerConnected = true;
+
+        public double pivotPosition = 0.0;
+        public double pivotVelocityMetersPerSec = 0.0;
 
         public double[] pivotAppliedVolts = new double[] {};
         public double[] pivotCurrentAmps = new double[] {};
 
-        public double climbIntakeVelocityRadPerSec = 0.0;
         public double climbIntakeVolts = 0.0;
         public double climbIntakeCurrentAmps = 0.0;
     }
@@ -20,10 +23,6 @@ public interface ClimberIO {
     default void updateInputs(ClimberIOInputsAutoLogged inputs) {}
 
     default void setPivotVoltage(double volts) {}
-
-    default void setPivotPosition(Rotation2d rotation) {}
-
-    default void resetPivotPosition(Rotation2d rotation) {}
 
     default void setIntakeVoltage(double volts) {}
 }
--- src/main/java/ca/warp7/frc2025/subsystems/Climber/ClimberIOSim.java
+++ src/main/java/ca/warp7/frc2025/subsystems/Climber/ClimberIOSim.java
@@ -2,12 +2,8 @@ package ca.warp7.frc2025.subsystems.Climber;
 
 import ca.warp7.frc2025.Constants.Climber;
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
@@ -32,27 +28,15 @@ public class ClimberIOSim implements ClimberIO {
             Climber.CLIMBER_INTAKE_GEAR_RATIO,
             0.0);
 
-    ProfiledPIDController pivotController = new ProfiledPIDController(
-            Climber.CLIMBER_PIVOT_kP,
-            Climber.CLIMBER_PIVOT_kI,
-            Climber.CLIMBER_PIVOT_kD,
-            new Constraints(10.00, 10.00));
-    ArmFeedforward pivotFeedForward =
-            new ArmFeedforward(Climber.CLIMBER_PIVOT_kS, Climber.CLIMBER_PIVOT_kV, Climber.CLIMBER_PIVOT_kA);
-
     @Override
     public void updateInputs(ClimberIOInputsAutoLogged inputs) {
         pivotSim.update(0.20);
         intakeSim.update(0.20);
 
-        inputs.pivotRotation = Rotation2d.fromRadians(pivotSim.getAngleRads());
-        inputs.pivotVelocityRadPerSec = pivotSim.getAngleRads();
-
         inputs.pivotAppliedVolts = new double[] {pivotAppliedVolts};
         inputs.pivotCurrentAmps = new double[] {pivotSim.getCurrentDrawAmps()};
 
         inputs.climbIntakeVolts = intakeSim.getInputVoltage();
-        inputs.climbIntakeVelocityRadPerSec = intakeSim.getAngularVelocityRPM();
         inputs.climbIntakeCurrentAmps = intakeSim.getCurrentDrawAmps();
     }
 
@@ -60,18 +44,6 @@ public class ClimberIOSim implements ClimberIO {
     public void setPivotVoltage(double volts) {
         pivotAppliedVolts = MathUtil.clamp(volts, -12, 12);
         pivotSim.setInputVoltage(pivotAppliedVolts);
-    }
-
-    @Override
-    public void setPivotPosition(Rotation2d rotation) {
-        setPivotVoltage(pivotController.calculate(pivotSim.getAngleRads(), rotation.getRadians())
-                + pivotFeedForward.calculate(
-                        pivotController.getSetpoint().position, pivotController.getSetpoint().velocity));
-    }
-
-    @Override
-    public void resetPivotPosition(Rotation2d rotation) {
-        pivotSim.setState(rotation.getRadians(), 0.0);
     }
 
     @Override
