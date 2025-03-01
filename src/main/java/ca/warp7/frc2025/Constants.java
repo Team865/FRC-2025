d2a3d40e96829a5ade32cccee306dfe25859fa2b
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-02-28 21:57:18 -0500

    Update constants

--- src/main/java/ca/warp7/frc2025/Constants.java
+++ src/main/java/ca/warp7/frc2025/Constants.java
@@ -61,8 +61,8 @@ public final class Constants {
         public static final Distance STOW = Inches.of(0);
         public static final Distance L4 = Inches.of(28.75);
         public static final Distance INTAKE = Inches.of(3);
-        public static final Distance L3 = Inches.of(21);
-        public static final Distance L2 = Inches.of(12.75);
+        public static final Distance L3 = Inches.of(19.625);
+        public static final Distance L2 = Inches.of(11.75);
     }
 
     public static final class Intake {
