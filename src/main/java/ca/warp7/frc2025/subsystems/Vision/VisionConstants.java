39f595346deaa00a4a081a7ee0b78b99da4200ba
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-03-04 12:43:13 -0500

    Fix camera position in sim

--- src/main/java/ca/warp7/frc2025/subsystems/Vision/VisionConstants.java
+++ src/main/java/ca/warp7/frc2025/subsystems/Vision/VisionConstants.java
@@ -68,9 +68,9 @@ public class VisionConstants {
 
     // Robot to camera transforms
     // (Not used by Limelight, configure in web UI instead)
-    public static Transform3d robotToCamera0 = new Transform3d(0.17858, 0.16464, 0.259, new Rotation3d(0.0, -13, 0.0));
+    public static Transform3d robotToCamera0 = new Transform3d(0.17858, -0.16464, 0.259, new Rotation3d(0.0, -13, 0.0));
     public static Transform3d robotToCamera1 = new Transform3d(
-            -0.17858, // -0.173683,
+            0.17858, // -0.173683,
             0.16464, // 0.178485,
             0.259, // 0.260248,
             // new Rotation3d(0.0, Units.degreesToRadians(-15), Units.degreesToRadians(15)));
