6ae64d783b1ea5fe44f70e6e6c962aab6d778fb6
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-03-04 23:41:48 -0500

    Decrease max current and wheel radius

--- src/main/java/ca/warp7/frc2025/generated/TunerConstants.java
+++ src/main/java/ca/warp7/frc2025/generated/TunerConstants.java
@@ -48,7 +48,7 @@ public class TunerConstants {
 
     // The stator current at which the wheels start to slip;
     // This needs to be tuned to your individual robot
-    private static final Current kSlipCurrent = Amps.of(120.0);
+    private static final Current kSlipCurrent = Amps.of(85.0);
 
     // Initial configs for the drive and steer motors and the azimuth encoder; these cannot be null.
     // Some configs will be overwritten; check the `with*InitialConfigs()` API documentation.
@@ -77,7 +77,7 @@ public class TunerConstants {
 
     private static final double kDriveGearRatio = 6.122448979591837;
     private static final double kSteerGearRatio = 21.428571428571427;
-    private static final Distance kWheelRadius = Inches.of(1.927);
+    private static final Distance kWheelRadius = Inches.of(1.895);
 
     private static final boolean kInvertLeftSide = false;
     private static final boolean kInvertRightSide = true;
