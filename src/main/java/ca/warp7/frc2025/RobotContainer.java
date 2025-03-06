feb25030c984b0e4e8c2699f2302ff2b869c2d5f
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-03-06 17:38:15 -0500

    Fix building

--- src/main/java/ca/warp7/frc2025/RobotContainer.java
+++ src/main/java/ca/warp7/frc2025/RobotContainer.java
@@ -44,6 +44,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
