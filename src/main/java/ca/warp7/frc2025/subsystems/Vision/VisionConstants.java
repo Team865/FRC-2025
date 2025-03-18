a7d617adf0f02bc5a91ea15859aeb790b6b73e49
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-03-18 13:46:39 -0400

    Fix unit conversion error in vision constants

--- src/main/java/ca/warp7/frc2025/subsystems/Vision/VisionConstants.java
+++ src/main/java/ca/warp7/frc2025/subsystems/Vision/VisionConstants.java
@@ -18,7 +18,8 @@ public class VisionConstants {
 
     // Robot to camera transforms
     // (Not used by Limelight, configure in web UI instead)
-    public static Transform3d robotToCamera0 = new Transform3d(0.17858, -0.16464, 0.259, new Rotation3d(0.0, -13, 0.0));
+    public static Transform3d robotToCamera0 =
+            new Transform3d(0.17858, -0.16464, 0.259, new Rotation3d(0.0, Units.degreesToRadians(-13), 0.0));
     public static Transform3d robotToCamera1 = new Transform3d(
             0.17858, // -0.173683,
             0.16464, // 0.178485,
