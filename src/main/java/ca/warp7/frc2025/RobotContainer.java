bb3cbbd346c9904a07288323df305d613f57b5a4
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-03-20 17:29:51 -0400

    Change algae clear voltage

--- src/main/java/ca/warp7/frc2025/RobotContainer.java
+++ src/main/java/ca/warp7/frc2025/RobotContainer.java
@@ -289,12 +289,12 @@ public class RobotContainer {
         Command algaeClearHigh = drive.setSpeedModifer(0.25)
                 .onlyIf(canMoveElevator)
                 .andThen(elevator.setGoal(Elevator.L2A))
-                .andThen(intake.setVoltsRoller(6));
+                .andThen(intake.setVoltsRoller(4.5));
 
         Command algaeClearLow = drive.setSpeedModifer(0.25)
                 .onlyIf(canMoveElevator)
                 .andThen(elevator.setGoal(Elevator.L1A))
-                .andThen(intake.setVoltsRoller(6));
+                .andThen(intake.setVoltsRoller(4.5));
 
         Command spitCoral = intake.setVoltsRoller(5)
                 .andThen(new WaitCommand(1))
