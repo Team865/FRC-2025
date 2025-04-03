8402a980f98b52193e39f45878b4e8d274e81b53
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-04-02 23:03:15 -0400

    Fix red human player math

--- src/main/java/ca/warp7/frc2025/FieldConstants.java
+++ src/main/java/ca/warp7/frc2025/FieldConstants.java
@@ -65,20 +65,23 @@ public class FieldConstants {
                 rightCenterFace.getX(),
                 fieldWidth - rightCenterFace.getY(),
                 Rotation2d.fromRadians(-rightCenterFace.getRotation().getRadians()));
+        // public static final Pose2d
         public static final Pose2d rightCenterFaceRed = new Pose2d(
                 fieldLength - rightCenterFace.getX(),
                 rightCenterFace.getY(),
-                Rotation2d.fromRadians(rightCenterFace
-                        .getRotation()
-                        .plus(Rotation2d.kCCW_90deg)
-                        .getRadians()));
+                Rotation2d.k180deg.minus(rightCenterFace.getRotation()));
+        // Rotation2d.fromRadians(rightCenterFace
+        //         .getRotation()
+        //         .plus(Rotation2d.kCCW_90deg)
+        //         .getRadians()));
         public static final Pose2d leftCenterFaceRed = new Pose2d(
                 rightCenterFaceRed.getX(),
                 leftCenterFace.getY(),
-                Rotation2d.fromRadians(leftCenterFace
-                        .getRotation()
-                        .minus(Rotation2d.kCCW_90deg)
-                        .getRadians()));
+                Rotation2d.k180deg.minus(leftCenterFace.getRotation()));
+        // Rotation2d.fromRadians(leftCenterFace
+        //         .getRotation()
+        //         .minus(Rotation2d.kCCW_90deg)
+        //         .getRadians()));
     }
 
     public static class Reef {
